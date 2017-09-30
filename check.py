4016aeca312551882c0596171afd0189436f3149
hidewon <hidewon@localhost.localdomain>
2017-09-30 19:41:40 +0900

    tensorflow

--- check.py
+++ check.py
@@ -27,3 +27,27 @@ def build_model():
 		optimizer='sgd',
 		metrics=['accuracy']
 	)
+	# read weighted datas of models for tensorflows.
+	model.load_weights('flower.hdf5')
+	return model
+
+def check(model,fname):
+	# normalize datas after load images.
+	img=Image.open(fname)
+	img=img.convert('RGB')
+	img=img.resize((photo_size,photo_size))
+	data=np.asarray(img).reshape((-1,data_size))/ 256
+
+	# predict name of flowers in the images.
+	res=model.predict([data])[0]
+	y=res.argmax() # answer (max values is answer.)
+	per=int(res[y] * 100) # get correct rate.
+	print("{0} ({1} %)".format(labels[y], per))
+
+if len(sys.argv) <= 1:
+	print('checy.py [filename]')
+	quit()
+
+model=build_model()
+check(model,sys.argv[1])
+
